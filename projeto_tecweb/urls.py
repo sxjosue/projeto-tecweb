2be84989e9f657ed58e6df39ba5a077767e0d712
sxjosue <133665545+sxjosue@users.noreply.github.com>
2023-08-22 18:19:50 -0400

    incio das views

--- projeto_tecweb/urls.py
+++ projeto_tecweb/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Polo_Amazontech.views import home #importa
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', home) #define a rota
 ]

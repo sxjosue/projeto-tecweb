1f797cfaa030d3ffdca676cfe19d3aac5294a728
sxjosue <133665545+sxjosue@users.noreply.github.com>
2023-08-28 17:18:13 -0400

    problema ao redirecionar ao login, fora isso cadastro e login ok!

--- Polo_Amazontech/urls.py
+++ Polo_Amazontech/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path("produtos.html/", views.produtos, name='produtos'),
-    path("produtos.html/cadastro.html/", views.cadastro, name='cadastro'),
+    path("produtos.html/cadastro.html/", views.cadastro, name='cadastro'), #cadastro de produto
+    path("signup.html/", views.signup, name='signup'), #cadastro do usário
+    path("login.html/", views.login, name='login'), #login do usuário
 ]
--- Polo_Amazontech/views.py
+++ Polo_Amazontech/views.py
@@ -1,6 +1,42 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as Login
+from django.contrib.auth.decorators import login_required
 from .form import ProdutoForm
 from .models import Produto #esse ponto indica que o models.py está no mesmo diretorio
+
+def signup(request):
+    if request.method == "GET":
+        return render(request, 'signup.html')
+    else:
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+
+        user = User.objects.filter(username=username).first()
+        if user:
+            return HttpResponse("Usuario ja cadastrado")
+
+        user = User.objects.create_user(username=username, email=email, password=senha) #parâmetros para criar o usuario
+        user.save()
+
+        return HttpResponse("Usuario cadastrado com sucesso")
+
+def login(request):
+    if request.method == "GET":
+        return render(request, 'login.html')
+    else:
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+
+        user = authenticate(username=username, password=senha)
+        if user:
+            Login(request, user)
+            return HttpResponse('Bem vindo!')
+        else:
+            return HttpResponse('Email ou senha invalidos')
 
 def index(request):
     return render(request, 'index.html')
@@ -10,6 +46,6 @@ def produtos(request):
     data['Produtos'] = Produto.objects.all() #mostra os objetos que já tem no banco
     return render(request, 'produtos.html', data)
 
+@login_required(login_url = "login.html/") #não aparecenas views isso aqui
 def cadastro(request):
-    form = ProdutoForm()
-    return render(request, 'cadastro.html', {'form':form})
+        return render(request, 'cadastro.html') #algumas coisas o usuario so pode acessar com login feito
